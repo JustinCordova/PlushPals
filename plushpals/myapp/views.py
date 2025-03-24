27d9a6d396527571d36fbe3107038dee588cf168
jasmrutt <jnr7@njit.edu>
2025-03-23 20:47:23 -0400

    added django framework, need to connect pages to url

---
+++ plushpals/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'myapp/index.html')
+
+def about(request):
+    return render(request, 'myapp/aboutpage.html')
