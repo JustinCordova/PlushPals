7d8baa357d6dab87eabeeea8f7ea5cbe74f6bf0f
Justin <justinavodroc@gmail.com>
2025-04-24 10:51:55 -0400

    Finalized Admin configuration, you can now manage products and feedback through admin portal

--- plushpals/store/admin.py
+++ plushpals/store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Feedback
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_featured')  # Show in list view
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}  # Auto-fill slug from name
+    list_filter = ('is_featured',)
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'rating', 'submitted_at')
+    list_filter = ('rating', 'submitted_at')
+    search_fields = ('user_name', 'user_email', 'message')
