57f3018d997f54eaa8d85603d9e7f234279c4085
Justin <justinavodroc@gmail.com>
2025-04-23 21:59:38 -0400

    Updated Product and Feedback models

--- plushpals/store/models.py
+++ plushpals/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -12,10 +14,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Feedback(models.Model):
     user_name = models.CharField(max_length=100)
+    # Email validator is automatically used, only add manually if you want to restrict a domain
     user_email = models.EmailField()
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    rating = models.IntegerField(
+        choices=[(i, i) for i in range(1, 6)],
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     message = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add=True)
 
